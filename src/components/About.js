92fe26f3dc2eb031d9e380ab295860fa666b171e
Nick Glushchenko <enayfls@gmail.com>
2022-01-20 17:01:32 +0200

    autoprefixer for css

--- src/components/About.js
+++ src/components/About.js
@@ -3,10 +3,11 @@ const About = () =>{
         <section className="about">
             <div className="inner">
                 <h2>About me </h2>
-                <p>I am a FrontEnd developer with 5 years of experience specializing in React and JavaScript, working on distributed, web-based systems and developing scalable user-centric web applications.</p>
-                <p>Worked both in a startup and in a fast-growing high profile web company(Natural Intelligence) I’ve gained the experience and personal skills necessary to deal with advanced technological challenges.</p>
-                <p>Led technology initiatives and improvements as a senior developer working on products with high UI/UX standards.</p>
-                <p>Throughout my career, I have been acknowledged as a quick learner, highly motivated team player with a passion for solving problems. I have a positive attitude and am always ready to accept new challenges.</p>
+                <p>I am a Front End developer with 5 years of experience. I specialize in React, JavaScript, and have worked with distributed web-based systems and developed scalable user-centric web applications.</p>
+                <p>I have worked in both a startup environment and in a fast-paced well-estbalished company. These companies and environments have allowed me to gain the experience and personal skills necessary to deal with advanced technological challenges.
+</p>
+                <p>In my position, as a Senior Developer, I led initiatives that streamlined our technology performance and executed product improvements with high UI/UX standards.</p>
+                <p>Throughout my career, I have been acknowledged as a quick learner, a highly motivated team player, and have a passion for solving problems. I have a positive attitude and am always ready to accept the next challenge.</p>
               </div>
         </section>
      );
